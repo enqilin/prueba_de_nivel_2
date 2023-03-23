b6bfd3442a50781e5689d601fb3aef012bea1927
nadous <114181724+enqilin@users.noreply.github.com>
2023-03-23 08:26:21 +0000

    la clase rectangulo

--- punto.py
+++ punto.py
@@ -20,13 +20,13 @@ y calcule la distancia entre los dos puntos y la muestre por pantalla."""
 
 
 
-import 
+import math
 
 class Punto:
     def __init__(self, x = 0 , y=0):
         self.x = x
         self.y = y
-    
+
     def __str__(self):
         return "el punto es P({},{}).".format(self.x , self.y)
 
@@ -52,6 +52,37 @@ class Punto:
         return "El vector es V({},{})".format(punto_x, punto_y)
 
     def distancia(self, punto):
-        punto_x = punto.x - self.x
-        punto_y = punto.y - self.y
-    
+        punto_x = (punto.x - self.x)**2
+        punto_y = (punto.y - self.y)**2
+        distance = math.sqrt(punto_x + punto_y)
+        return "La distancia es {}".format(distance)
+
+
+
+"""• Crea una clase llamada Rectangulo con dos puntos (inicial y final) 
+que formarán la diagonal del rectángulo.
+• Añade un método constructor para crear ambos puntos fácilmente, 
+si no se envían se crearán dos puntos en el origen por defecto.
+• Añade al rectángulo un método llamado base que muestre la base.
+• Añade al rectángulo un método llamado altura que muestre la altura.
+• Añade al rectángulo un método llamado area que muestre el area"""
+
+class Rectangulo(Punto):
+    def __init__(self, x2 = 0 ,y2 = 0):
+        super().__init__(self)
+        self.x2 = x2
+        self.y2 = y2
+
+    def base(self, punto):
+        base = abs(self.x -punto.x)
+        return "la base del rectangulo es {}".format(base)
+
+    def altura(self,punto):
+        altura = abs(self.y - punto.y)
+        return "La altura es {}".format(altura)
+
+    def area(self,punto):
+        base = abs(self.x -punto.x)
+        altura = abs(self.y - punto.y)
+        area = base * altura
+        return "El area es{}".format(area)
---
+++ test_punto.py
@@ -0,0 +1,1 @@
+form punto import Punto
